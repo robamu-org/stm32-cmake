ac4ddbc6be1516548bd8659128afe6d804942ee0
Robin Mueller <robin.mueller.m@gmail.com>
2021-07-13 11:12:33 +0200

    no strict aliasing optional
    Furthermore, not enforcing compiler flags anymore

--- cmake/stm32/utilities.cmake
+++ cmake/stm32/utilities.cmake
@@ -13,10 +13,11 @@ function(stm32_util_create_family_targets FAMILY)
 
     option(STM32_ENABLE_FAST_MATH "Add -ffast-math compiler flag" OFF)
     option(STM32_NO_BUILTIN "Add -fno-builtin compiler flag" OFF)
+    option(STM32_NO_STRICT_ALIASING "Add -fno-strict-aliasing compiler flag" OFF)
 
     if(NOT (TARGET STM32::${FAMILY}${CORE_C}))
         set(STM32_COMPILE_OPTIONS
-            -mthumb -mabi=aapcs -Wall -ffunction-sections -fdata-sections -fno-strict-aliasing
+            -mthumb -mabi=aapcs -Wall -ffunction-sections -fdata-sections
         )
         if(STM32_ENABLE_FAST_MATH)
             list(APPEND STM32_COMPILE_OPTIONS -ffast-math)
@@ -24,19 +25,18 @@ function(stm32_util_create_family_targets FAMILY)
         if(STM32_NO_BUILTIN)
             list(APPEND STM32_COMPILE_OPTIONS -fno-builtin)
         endif()
+        if(STM32_NO_STRICT_ALIASING)
+            list(APPEND STM32_COMPILE_OPTIONS -fno-strict-aliasing)
+        endif()
 
         add_library(STM32::${FAMILY}${CORE_C} INTERFACE IMPORTED)
         target_compile_options(STM32::${FAMILY}${CORE_C} INTERFACE 
             --sysroot="${TOOLCHAIN_SYSROOT}"
             ${STM32_COMPILE_OPTIONS}
-            $<$<CONFIG:Debug>:-Og>
-            $<$<CONFIG:Release>:-Os>
         )
         target_link_options(STM32::${FAMILY}${CORE_C} INTERFACE 
             --sysroot="${TOOLCHAIN_SYSROOT}"
             -mthumb -mabi=aapcs -Wl,--gc-sections
-            $<$<CONFIG:Debug>:-Og>
-            $<$<CONFIG:Release>:-Os -s>
         )
         target_compile_definitions(STM32::${FAMILY}${CORE_C} INTERFACE 
             STM32${FAMILY}
